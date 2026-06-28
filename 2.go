d6f3ed9636194a0fd6c5199967e7cd6b2a1a887a
Vitaly <vitalik.emperor@gmail.com>
2026-06-29 00:14:37 +0500

    first commit

---
+++ 2.go
@@ -0,0 +1,36 @@
+package main
+
+import "fmt"
+
+// Определяем структуру Person
+type Person struct {
+	Name string
+	Age  int
+}
+
+// Создаём слайс людей
+var people []Person
+
+// Функция для добавления человека в слайс
+func addPerson(name string, age int) {
+	person := Person{
+		Name: name,
+		Age:  age,
+	}
+	people = append(people, person)
+}
+
+// Функция для вывода списка людей
+func printPeople() {
+	for _, person := range people {
+		fmt.Printf("Имя: %s, Возраст: %d\n", person.Name, person.Age)
+	}
+}
+
+func main() {
+	addPerson("Алексей", 30)
+	addPerson("Мария", 25)
+	addPerson("Дмитрий", 40)
+
+	printPeople()
+}
---
+++ Var&const&github.go
@@ -0,0 +1,10 @@
+package main
+
+import "fmt"
+
+func main() {
+	number := 10     //declaring and assigning a number to a variable
+	name := "Vitaly" //declaring and assigning a name to a variable
+	fmt.Println("number:", number)
+	fmt.Println("name:", name)
+}

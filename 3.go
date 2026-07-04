1b059691c27c2a81a178260439dcfc6c2196570b
Vitaly <vitalik.emperor@gmail.com>
2026-07-04 13:43:12 +0500

    The solution to the first problem with negative and positive numbers

--- 3.go
+++ 3.go
@@ -1,3 +1,22 @@
 package main
 
-//123
+import "fmt"
+
+func main() {
+	var number int
+	fmt.Print("Введите число: ")
+	_, err := fmt.Scan(&number)
+	if err != nil {
+		fmt.Println("Ошибка ввода.")
+		return
+	}
+
+	if number < 0 {
+		fmt.Println("This number is less than zero")
+	}
+
+	if number > 0 {
+		fmt.Println("This number is greater than zero")
+	}
+
+}
